ca14cee5b70efaaa6323d4b86b436adb6977ea95
helldriver <nbatkins@gmail.com>
2017-10-05 22:00:35 -0500

    Dot net core 2.0 spa for vue with oauth
    Dot net core 2.0 spa for vue with oauth

---
+++ api/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetCoreReactTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls(config["serverBindingUrl"])
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
